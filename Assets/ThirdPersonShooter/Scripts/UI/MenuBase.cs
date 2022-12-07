0e8935800530e84370af814312eed6a13aa00724
Jack Aylward <s214520@students.aie.edu.au>
2022-12-07 16:51:18 +1100

    quicksave

---
+++ Assets/ThirdPersonShooter/Scripts/UI/MenuBase.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace ThirdPersonShooter.UI
+{
+	public abstract class MenuBase : MonoBehaviour
+	{
+		public bool IsDefault => isDefault;
+		public bool isVisile => gameObject.activeSelf;
+		
+		public abstract string ID { get; }
+		[SerializeField] private bool isDefault;
+
+		public void SetVisible(bool _visisble) => gameObject.SetActive(_visisble);
+
+		public virtual void OnOpenMenu(UIManager _manager) { }
+		public virtual void OnCloseMenu(UIManager _manager) { }
+	}
+}
