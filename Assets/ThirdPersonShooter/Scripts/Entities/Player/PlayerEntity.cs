5f673c11b9ec6262fb8b45b13d00202489edafd1
Jack Aylward <s214520@students.aie.edu.au>
2022-12-08 16:46:34 +1100

    save

--- Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
+++ Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
+using ThirdPersonShooter.UI;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace ThirdPersonShooter.Entities.Player
 {
@@ -9,11 +12,61 @@ namespace ThirdPersonShooter.Entities.Player
 		public ref Stats Stats => ref stats;
 		public Vector3 Position => transform.position;
 
+		public Action<int> onScoreUpdated;
+
 		[SerializeField] private Stats stats;
+		[SerializeField] private InputActionReference pauseAction;
+
+		[Header("Components")]
+		[SerializeField] private PlayerInput input;
+		[SerializeField] private Weapon weapon;
+		[SerializeField] private AudioSource hurtSource;
+		[SerializeField] private AudioSource deathSource;
+
+		public int Score => score;
+		
+		private int score;
+
+		public void AddScore(int _value)
+		{
+			score += _value;
+			onScoreUpdated?.Invoke(score);
+		}
 
 		private void Awake()
 		{
 			stats.Start();
+
+			stats.onDeath += OnDeath;
+			stats.onHealthChanged += OnHealthChange;
+			
+			weapon.SetPlayer(this);
+			
+			if(GameManager.IsValid())
+				GameManager.Instance.Player = this;
+
+			if(UIManager.IsValid())
+				input.uiInputModule = UIManager.Instance.inputModule;
 		}
+
+		private void OnDestroy()
+		{
+			stats.onDeath -= OnDeath;
+			stats.onHealthChanged -= OnHealthChange;
+		}
+
+		private void OnEnable() => pauseAction.action.performed += OnPausePerformed;
+
+		private void OnDisable() => pauseAction.action.performed -= OnPausePerformed;
+
+		private void OnPausePerformed(InputAction.CallbackContext _context)
+		{
+			if(GameManager.IsValid())
+				GameManager.Instance.TogglePaused();
+		}
+
+		private void OnDeath() => deathSource.Play();
+
+		private void OnHealthChange(float _health) => hurtSource.Play();
 	}
 }
--- Assets/ThirdPersonShooter/Scripts/UI/GameOver.cs
+++ Assets/ThirdPersonShooter/Scripts/UI/GameOver.cs
@@ -1,23 +1,33 @@
-﻿using UnityEngine;
+﻿using TMPro;
+
+using UnityEngine;
 
 namespace ThirdPersonShooter.UI
 {
 	public class GameOver : MenuBase
 	{
 		public override string ID => "Game Over";
+
+		[SerializeField] private TextMeshProUGUI scoreText;
 		public override void OnOpenMenu(UIManager _manager)
 		{
 			Cursor.visible = true;
 			Cursor.lockState = CursorLockMode.None;
+
+			scoreText.text = $"Score: {GameManager.Instance.Player.Score}";
+			
+			GameManager.Instance.TogglePaused();
 		}
 
 		public override void OnCloseMenu(UIManager _manager)
 		{
 			Cursor.visible = false;
 			Cursor.lockState = CursorLockMode.Locked;
+			scoreText.text = "Score: 0";
+
+			GameManager.Instance.TogglePaused();
 		}
 
 		public void OnClickMainMenu() => GameManager.Instance.LevelManager.UnloadGame(() => UIManager.ShowMenu("Main", false));
-
 	}
 }
